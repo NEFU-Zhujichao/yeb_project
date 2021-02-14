52346f7009b4e37fb9dd59df659fc79fc76f8b8c
13949 <1394949540@qq.com>
2021-02-14 20:09:32 +0800

    init

--- yeb-server/src/main/java/com/example/yebserver/YebServerApplication.java
+++ yeb-server/src/main/java/com/example/yebserver/YebServerApplication.java
@@ -1,9 +1,11 @@
 package com.example.yebserver;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.yebserver.mapper")
 public class YebServerApplication {
 
     public static void main(String[] args) {
