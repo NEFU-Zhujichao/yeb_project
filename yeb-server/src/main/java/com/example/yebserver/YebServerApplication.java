f44d86cdf4c70f250c9a743fc247850c07fb2194
13949 <1394949540@qq.com>
2021-02-14 20:02:55 +0800

    init

---
+++ yeb-server/src/main/java/com/example/yebserver/YebServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.yebserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YebServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YebServerApplication.class, args);
+    }
+
+}
---
+++ yeb-server/src/test/java/com/example/yebserver/YebServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yebserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YebServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
