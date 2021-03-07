ada9195a05ba73b0d97fc08dd6ff85b44ed2dde3
13949 <1394949540@qq.com>
2021-03-07 16:14:57 +0800

    new feature

--- yeb-server/src/main/java/com/example/yebserver/controller/SalaryController.java
+++ yeb-server/src/main/java/com/example/yebserver/controller/SalaryController.java
@@ -1,9 +1,14 @@
 package com.example.yebserver.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.example.yebserver.pojo.RespBean;
+import com.example.yebserver.pojo.Salary;
+import com.example.yebserver.service.ISalaryService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 /**
  * <p>
@@ -14,7 +19,42 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2021-02-16
  */
 @RestController
-@RequestMapping("/salary")
+@RequestMapping("/salary/sob")
 public class SalaryController {
 
+    @Autowired
+    private ISalaryService salaryService;
+
+    @ApiOperation(value = "获取所有工资账套")
+    @GetMapping("/")
+    public List<Salary> getAllSalaries(){
+        return salaryService.list();
+    }
+
+    @ApiOperation(value = "添加工资账套")
+    @PostMapping("/")
+    public RespBean addSalary(@RequestBody Salary salary){
+        if (salaryService.save(salary)){
+            return RespBean.success("添加成功");
+        }
+        return RespBean.error("添加失败");
+    }
+
+    @ApiOperation(value = "修改工资账套")
+    @PutMapping("/")
+    public RespBean updateSalary(@RequestBody Salary salary){
+        if (salaryService.updateById(salary)){
+            return RespBean.success("修改成功");
+        }
+        return RespBean.error("修改失败");
+    }
+
+    @ApiOperation(value = "删除工资账套")
+    @DeleteMapping("/{id}")
+    public RespBean deleteSalary(@PathVariable Integer id){
+        if (salaryService.removeById(id)){
+            return RespBean.success("删除成功");
+        }
+        return RespBean.error("删除失败");
+    }
 }
